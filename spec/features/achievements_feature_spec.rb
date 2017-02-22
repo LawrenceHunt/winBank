aca59e8103836c210746204acb4afc0d5e55385f
Chris Cooper <cjcooper252@gmail.com>
2017-02-22 16:07:05 +0000

    adds users index page at login

--- spec/features/achievements_feature_spec.rb
+++ spec/features/achievements_feature_spec.rb
@@ -21,7 +21,6 @@ feature 'achievements' do
   end
 
  context 'achievement added with leadership tag' do
-
     scenario 'should show that the achievement is a leadership achievement' do
       user = User.create(email: 'test@test.com', password: 'password', id: 0)
       user.achievements.create(description: 'Test description', leadership: true)
@@ -30,11 +29,9 @@ feature 'achievements' do
       expect(page).to have_content 'Test description'
       expect(page).to have_content 'Leadership'
     end
-
   end
 
 context 'achievement added with efficiency tag' do
-
     scenario 'should show that the achievement is an efficiency achievement' do
       user = User.create(email: 'test@test.com', password: 'password', id: 0)
       user.achievements.create(description: 'Test description', efficiency: true)
@@ -45,30 +42,22 @@ context 'achievement added with efficiency tag' do
   end
 
   context 'achievement added with both efficiency and leadership tag' do
-
     scenario 'should show that the achievement has both tags' do
       user = User.create(email: 'test@test.com', password: 'password', id: 0)
       user.achievements.create(description: 'Efficient Leadership Achievement', efficiency: true, leadership:true)
       visit 'users/0'
       expect(page).to have_content 'Efficiency'
       expect(page).to have_content 'Leadership'
-
     end
-
   end
 
   context 'see the number of achievements with each tag' do
-
     scenario 'show a count for leadership achievements' do
       user = User.create(email: 'test@test.com', password: 'password', id: 0)
       user.achievements.create(description: 'Efficient Leadership Achievement', efficiency: true, leadership:true)
       visit 'users/0'
       expect(page).to have_content("Theme Counts:")
       expect(page).to have_content("Leadership: 1")
-      save_and_open_page
     end
-
   end
-
-
 end
