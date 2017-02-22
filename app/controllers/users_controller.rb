cfe92ba3bba8ba15ae20fe2694552f22ac4d07ad
Oscar Barlow <oscar.barlow@gmail.com>
2017-02-22 16:30:06 +0000

    make mentorship achievements be counted

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,10 +13,11 @@ class UsersController < ApplicationController
   private
 
   def themes_count(achievements_array)
-    counts = {leadership: 0, efficiency: 0}
+    counts = {leadership: 0, efficiency: 0, mentorship: 0}
     achievements_array.each do |achievement|
       counts[:leadership] += 1 if achievement.leadership
       counts[:efficiency] += 1 if achievement.efficiency
+      counts[:mentorship] += 1 if achievement.mentorship
     end
     counts
   end
